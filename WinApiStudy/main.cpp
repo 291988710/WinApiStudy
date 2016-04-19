375ec7a14668713390841941b9244f2faa6b8d96
291988710 <291988719@qq.com>
2016-04-19 19:36:56 +0800

    Hello function
    Hello windows api的函数。

--- WinApiStudy/main.cpp
+++ WinApiStudy/main.cpp
@@ -6,3 +6,11 @@
 	Function: Windows api study
 */
 /************************************************************************/
+
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+	MessageBox(NULL, TEXT("Hello, windows api!"), TEXT("HelloMsg"), 0);
+	return 0;
+}
